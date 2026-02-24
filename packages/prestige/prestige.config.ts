bd3501c6fb80684c2c981508a0cf8ae7cc2e8c58
luka onikadze <lukaonik@gmail.com>
2026-02-24 10:53:57 +0400

    fix failing tests

---
+++ packages/prestige/prestige.config.ts
@@ -0,0 +1,25 @@
+import { defineConfig } from "../src/vite";
+
+export default defineConfig({
+  title: "Title",
+  docsDir: "playground/src/content/docs",
+  description: " ee qweqwqeqeq  qweqw. ",
+  sidebar: [
+    {
+      label: "Text",
+      type: "link",
+      slug: "/demo",
+    },
+
+    {
+      label: "opaaaa",
+      type: "link",
+      slug: "/demo",
+    },
+    {
+      label: "opaa22aa",
+      type: "link",
+      slug: "/demo",
+    },
+  ],
+});

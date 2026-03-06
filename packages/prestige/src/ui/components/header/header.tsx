6021d654bd9627030eb83dab515028b8c931fb22
sopo <sofiko.imnaishvili@gmail.com>
2026-03-06 21:34:41 +0400

    update active link color

--- packages/prestige/src/ui/components/header/header.tsx
+++ packages/prestige/src/ui/components/header/header.tsx
@@ -1,4 +1,4 @@
-import { Link } from "@tanstack/react-router";
+import { Link} from "@tanstack/react-router";
 import collections from "virtual:prestige/collection-all";
 import { GitHub } from "../github/github";
 import { Search } from "../search/search";
@@ -8,18 +8,16 @@ export default function Header() {
   return (
       <header className="sticky top-0 z-40 flex h-header  border-b border-gray-200 bg-gray-50/80 px-4 backdrop-blur-md">
         <div className="container mx-auto flex max-w-360  items-center justify-between">
-          <div className="flex gap-4">
+          <div className="flex gap-2">
             <Link 
-            className="text-sm"
+            className="text-sm px-2 py-1 rounded hover:bg-zinc-100"
             to={"/"}>Home</Link>
             {collections.map((collection) => (
               <Link
                 key={collection.id}
                 to={`/${collection.id}` as any}
-                className="border-b-transparent border-b-2 text-sm"
-                activeProps={{
-                  style: { borderBottomColor: "var(--color-primary)" }, // style object is used to overide border transparent
-                }}
+                className="border-b-transparent border-b-2 text-sm px-2 py-1 rounded hover:bg-zinc-100 active:bg-zinc-200"
+                activeProps={{ className: "text-zinc-800 font-medium" }}
               >
                 {collection.label}
               </Link>

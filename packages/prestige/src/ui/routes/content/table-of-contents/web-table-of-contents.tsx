6693e10272bbfc1e92403f151ab82d89183e0075
luka onikadze <lukaonik@gmail.com>
2026-03-07 20:06:08 +0400

    update data

--- packages/prestige/src/ui/routes/content/table-of-contents/web-table-of-contents.tsx
+++ packages/prestige/src/ui/routes/content/table-of-contents/web-table-of-contents.tsx
@@ -9,8 +9,8 @@ export function WebTableOfContent({ toc }: { toc: TocItem[] }) {
   }
 
   return (
-    <nav className="sticky top-24 max-h-[calc(100vh-6rem)] overflow-y-auto w-64 shrink-0 hidden lg:block">
-     <span className="text-xs font-mono tracking-widest">ON THIS PAGE</span>
+    <nav className="sticky top-header h-main pt-4 overflow-y-auto w-64 shrink-0 hidden lg:block">
+      <span className="text-xs font-mono tracking-widest">ON THIS PAGE</span>
       <ul className="text-sm my-4">
         {toc.map((item) => (
           <li
@@ -21,7 +21,9 @@ export function WebTableOfContent({ toc }: { toc: TocItem[] }) {
               href={`${item.href}`}
               className={clsx(
                 "border-l py-1 px-4 block hover:text-gray-700 dark:hover:text-slate-100 transition-colors duration-200 line-clamp-2",
-                activeId === item.href ? "text-gray-700 border-gray-600 font-medium" : "text-gray-500  border-gray-200 ",
+                activeId === item.href
+                  ? "text-gray-700 border-gray-600 font-medium"
+                  : "text-gray-500  border-gray-200 ",
               )}
               onClick={(e) => handleLinkClick(e, item.href)}
             >

df45c9161ff8e2acaf212eff25883bc7432a60b8
luka onikadze <lukaonik@gmail.com>
2026-03-08 13:41:54 +0400

    create core route

--- packages/prestige/src/ui/index.ts
+++ packages/prestige/src/ui/index.ts
@@ -1,5 +1,5 @@
-export { default as Header } from "./components/header/header";
-export { PrestigePage, type PrestigePageProps } from "./components/prestige-page";
+export { default as Header } from "./core/header/header";
+export { PrestigePage, type PrestigePageProps } from "./core/prestige-page";
 export { CollectionRoute } from "./routes/collection/collection.route";
 export { ContentRoute } from "./routes/content/content.route";
 export { default as NotFound } from "./routes/not-found";

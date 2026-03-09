1e2d627152295fc02c84e1d66e6c7fc545038f1e
luka <lukaonik@gmail.com>
2026-03-09 13:10:48 +0400

    add footer license

--- packages/prestige/src/ui/core/footer/footer.tsx
+++ packages/prestige/src/ui/core/footer/footer.tsx
@@ -6,12 +6,12 @@ function License({ license }: { license: FooterProps["license"] }) {
   if (!license) {
     return null;
   }
-  return <a href={license.url}>{license.label}</a>;
+  return <a href={license.url}>Released under: {license.label}</a>;
 }
 
 export default function Footer({ license }: FooterProps) {
   return (
-    <footer className="mt-10 flex items-center justify-center border-t border-t-default-100 pt-10">
+    <footer className="mt-10 flex items-center justify-center border-t border-t-default-100 pt-10 bg-default-50 text-sm py-10 text-default-700">
       <License license={license} />
     </footer>
   );

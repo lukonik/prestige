562754d53c0dd446147d399d54eb4353c9e920be
luka <lukaonik@gmail.com>
2026-03-10 12:51:39 +0400

    update index

--- packages/prestige/playground/src/index.tsx
+++ packages/prestige/playground/src/index.tsx
@@ -1,8 +1,6 @@
-import "@fontsource/geist-sans";
-import "@fontsource/geist-mono";
+import { RouterProvider } from "@tanstack/react-router";
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import { RouterProvider } from "@tanstack/react-router";
 import { getRouter } from "./router";
 const router = getRouter();
 

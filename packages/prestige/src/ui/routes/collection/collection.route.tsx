d5ceb3edf26987da498094d1e50e9b7dd7ad505d
luka <lukaonik@gmail.com>
2026-03-10 10:47:06 +0400

    fix default link

--- packages/prestige/src/ui/routes/collection/collection.route.tsx
+++ packages/prestige/src/ui/routes/collection/collection.route.tsx
@@ -1,19 +1,10 @@
-import { Outlet, ParsedLocation, redirect } from "@tanstack/react-router";
+import { Outlet } from "@tanstack/react-router";
 import { SidebarType } from "../../../vite/core/content/content.types";
 import MobileSidebar from "./mobile-sidebar";
 import Sidebar from "./sidebar";
 
 export function CollectionRoute(sidebar: SidebarType, id: string) {
   return {
-    beforeLoad: ({ location }: { location: ParsedLocation }) => {
-      if (id === location.pathname.slice(1)) {
-        if (sidebar.defaultLink) {
-          throw redirect({
-            to: "/" + sidebar.defaultLink,
-          });
-        }
-      }
-    },
     component: () => {
       return (
         <div className="container mx-auto flex max-w-360 lg:gap-6">

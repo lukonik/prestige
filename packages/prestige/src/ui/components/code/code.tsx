7c444b2557c150a5b5ad2a7a4f587b8a22ce12e2
luka <lukaonik@gmail.com>
2026-03-11 17:38:13 +0400

    fix: set styles object to undefined for code elements

--- packages/prestige/src/ui/components/code/code.tsx
+++ packages/prestige/src/ui/components/code/code.tsx
@@ -1,3 +1,4 @@
+import clsx from "clsx";
 import { Highlight } from "prism-react-renderer";
 
 export interface CodeProps {
@@ -9,15 +10,21 @@ export function Code({ code, language = "tsx" }: CodeProps) {
   // Use a fragment or div, but ensure `render` actually exists before injecting
   return (
     <Highlight code={code} language={language}>
-      {({ style, tokens, getLineProps, getTokenProps }) => (
-        <pre className="not-prose" style={style}>
-          {tokens.map((line, i) => (
-            <div key={i} {...getLineProps({ line })}>
-              {line.map((token, key) => (
-                <span key={key} {...getTokenProps({ token })} />
-              ))}
-            </div>
-          ))}
+      {({ className, tokens, getLineProps, getTokenProps }) => (
+        <pre className={clsx("not-prose", className)}>
+          <code className={clsx("code-highlight", className)}>
+            {tokens.map((line, i) => (
+              <div key={i} {...getLineProps({ line })} style={undefined}>
+                {line.map((token, key) => (
+                  <span
+                    key={key}
+                    {...getTokenProps({ token })}
+                    style={undefined}
+                  />
+                ))}
+              </div>
+            ))}
+          </code>
         </pre>
       )}
     </Highlight>

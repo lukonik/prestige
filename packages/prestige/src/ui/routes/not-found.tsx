962aa5a95e2eaf9cca97858ecd879e7bcbb3179d
Luka Onikadze <81145822+lukonik@users.noreply.github.com>
2026-03-12 17:48:00 +0400

    feat: add not-found page (#77)

--- packages/prestige/src/ui/routes/not-found.tsx
+++ packages/prestige/src/ui/routes/not-found.tsx
@@ -1,3 +1,10 @@
-export default function NotFound() {
-  return <h1>This page could not be found.</h1>;
+export function PrestigeNotFound() {
+  return (
+    <div className="w-full h-full flex justify-center pt-15   lg:pt-30 ">
+      <div className="flex flex-col gap-3 items-center">
+        <h1 className="text-3xl lg:text-7xl">404</h1>
+        <h2 className="text-3xl lg:text-5xl">Page Not Found</h2>
+      </div>
+    </div>
+  );
 }

015f41d1c63d7eaf2735d425d421864d3f86f21a
Luka Onikadze <81145822+lukonik@users.noreply.github.com>
2026-03-13 10:02:57 +0400

    docs: add docs project (#78)

---
+++ apps/docs/vite.config.ts
@@ -0,0 +1,42 @@
+import { prestige } from "@lonik/prestige/vite";
+import { defineConfig } from 'vite'
+import { devtools } from '@tanstack/devtools-vite'
+import tsconfigPaths from 'vite-tsconfig-paths'
+
+import { tanstackStart } from '@tanstack/react-start/plugin/vite'
+
+import viteReact from '@vitejs/plugin-react'
+import tailwindcss from '@tailwindcss/vite'
+import { nitro } from 'nitro/vite'
+
+const config = defineConfig({
+  plugins: [
+    prestige({
+      title: "Prestige",
+      license: {
+          label: "MIT",
+          url: "https://opensource.org/licenses/MIT",
+        },
+      github: "https://github.com/lukonik/prestige",
+      collections: [
+        {
+          id: "docs",
+          items: [
+            {
+              label: "Introduction",
+              slug: "docs/introduction",
+            }
+          ]
+        }
+      ]
+    }),
+    devtools(),
+    nitro({ rollupConfig: { external: [/^@sentry\//] } }),
+    tsconfigPaths({ projects: ['./tsconfig.json'] }),
+    tailwindcss(),
+    tanstackStart(),
+    viteReact(),
+  ],
+})
+
+export default config

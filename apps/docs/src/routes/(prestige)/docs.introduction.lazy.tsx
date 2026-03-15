d624ca776bd4a8158644b165757ff7846c82d13a
luka onikadze <lukaonik@gmail.com>
2026-03-15 10:46:21 +0400

    merge docs

--- apps/docs/src/routes/(prestige)/docs.introduction.lazy.tsx
+++ apps/docs/src/routes/(prestige)/docs.introduction.lazy.tsx
@@ -1,5 +1,5 @@
 import { createLazyFileRoute } from "@tanstack/react-router";
 import * as contentData from "virtual:prestige/content/docs/introduction";
-import { LazyContentRoute } from "@lonik/prestige/ui";
+import { ContentRoute } from "@lonik/prestige/ui";
 
-export const Route = createLazyFileRoute('/(prestige)/docs/introduction')(LazyContentRoute(contentData));
+export const Route = createLazyFileRoute('/(prestige)/docs/introduction')(ContentRoute(contentData));

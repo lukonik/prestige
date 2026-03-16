c8728af8260a3a5f14e6a59be48758b7dd7e6a76
luka <lukaonik@gmail.com>
2026-03-16 11:16:17 +0400

    update docs

--- apps/docs/src/routes/(prestige)/docs.reference.frontmatter-reference.lazy.tsx
+++ apps/docs/src/routes/(prestige)/docs.reference.frontmatter-reference.lazy.tsx
@@ -1,5 +1,5 @@
 import { createLazyFileRoute } from "@tanstack/react-router";
 import * as contentData from "virtual:prestige/content/docs/reference/frontmatter-reference";
-import { LazyContentRoute } from "@lonik/prestige/ui";
+import { ContentRoute } from "@lonik/prestige/ui";
 
-export const Route = createLazyFileRoute('/(prestige)/docs/reference/frontmatter-reference')(LazyContentRoute(contentData));
+export const Route = createLazyFileRoute('/(prestige)/docs/reference/frontmatter-reference')(ContentRoute(contentData));

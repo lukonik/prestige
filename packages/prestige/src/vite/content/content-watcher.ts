7b3ed4891d047373c8598482003e6f55e86f0104
Luka Onikadze <81145822+lukonik@users.noreply.github.com>
2026-03-17 19:04:27 +0400

    fix: Integrate native content watcher

--- packages/prestige/src/vite/content/content-watcher.ts
+++ packages/prestige/src/vite/content/content-watcher.ts
@@ -1,26 +1,26 @@
-import chokidar from "chokidar";
 import debounce from "debounce";
+import { join } from "pathe";
+import picomatch from "picomatch";
+import { ViteDevServer } from "vite";
 
-export function initContentWatcher(contentDir: string, onUpdate: () => void) {
+export function initContentWatcher(contentDir: string, server: ViteDevServer) {
+  const isDocsMatcher = picomatch(join(contentDir, "**/*.{md,mdx}"));
+
   const UPDATE_INTERVAL = 1000;
-  const scheduleUpdate = debounce(onUpdate, UPDATE_INTERVAL);
-
-  const watcher = chokidar
-    .watch(contentDir, {
-      awaitWriteFinish: true,
-      ignoreInitial: true,
-    })
-    .on("all", (event) => {
-      if (event !== "add" && event !== "unlink") {
-        return;
-      }
-
-      // Coalesce bursts of filesystem events into one update run per interval.
-      scheduleUpdate();
-    });
-
-  return () => {
-    scheduleUpdate.clear();
-    return watcher.close();
+  const schedule = debounce(() => {
+    server.restart();
+  }, UPDATE_INTERVAL);
+  const onFileChange = (file: string) => {
+    if (!isDocsMatcher(file)) {
+      return;
+    }
+    schedule();
   };
+  server.watcher.add(contentDir);
+  server.watcher.on("add", onFileChange);
+  server.watcher.on("unlink", onFileChange);
+
+  server.httpServer?.on("close", () => {
+    schedule.clear();
+  });
 }
